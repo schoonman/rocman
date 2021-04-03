094565bff007fef72e483ba3f5ddc3161b23d175
schoonman <cmschoonman@gmail.com>
2021-04-03 17:48:35 +0200

    added generic scoreboard

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+let score = 0
 level1Grid=[
     'p','r','r','r','r','r','r','r','r','r','r','r','r','r','r','r','r','r','r','g',
     'w','r','w','w','w','w','w','w','w','w','w','w','w','w','w','w','w','r','w','r',
@@ -26,7 +27,7 @@ function setupGrid(width,height) {
     const gridSize= width*height;
     const cellStates=[];
     const gridCells=[];
-
+    
     for (let i = 0; i<gridSize; i++) {
         const cell = document.createElement("div");
         cell.style.width=(100/width).toString(10)+"%";
@@ -51,34 +52,38 @@ function setupLevel(cellStates, levelGrid) {
             case 'p':
                 cellStates[i].classList.add("player");
                 break;
-            case 'r':
-                cellStates[i].classList.add("rock");
-                break;
-            case 'w':
-                cellStates[i].classList.add("wall");
-                break;
-            case 'g':
-                cellStates[i].classList.add("gem");
-                break;
-        }
-    })
-    return cellStates;
-}
-
-function movePlayer(playerPosition, i, gridCells, score) {
-    gridCells[playerPosition].classList.remove("player")
-    playerPosition+=i
-    gridCells[playerPosition].classList.add("player")
-    if (gridCells[playerPosition].classList.contains("rock")) {
-        score += 1; 
-        gridCells[playerPosition].classList.remove("rock");
-    } else if (gridCells[playerPosition].classList.contains("gem")) {
-        score += 10; 
-        gridCells[playerPosition].classList.remove("gem");
-    }
-    // console.log(playerPosition)
-    return [playerPosition,score]
-};
+                case 'r':
+                    cellStates[i].classList.add("rock");
+                    break;
+                    case 'w':
+                        cellStates[i].classList.add("wall");
+                        break;
+                        case 'g':
+                            cellStates[i].classList.add("gem");
+                            break;
+                        }
+                    })
+                    return cellStates;
+                }
+                
+                function movePlayer(playerPosition, i, gridCells,scoreDisplay) {
+                    console.log(scoreDisplay)
+                    gridCells[playerPosition].classList.remove("player")
+                    playerPosition+=i
+                    gridCells[playerPosition].classList.add("player")
+                    if (gridCells[playerPosition].classList.contains("rock")) {
+                        score += 1; 
+                        gridCells[playerPosition].classList.remove("rock");
+                    } else if (gridCells[playerPosition].classList.contains("gem")) {
+                        score += 10; 
+                        gridCells[playerPosition].classList.remove("gem");
+                    }
+                    scoreDisplay.innerHTML = score
+                    // console.log(score)
+                    // console.log(playerPosition)
+                    return [playerPosition,score]
+                };
+                
 
 function loadGame() {
     const width = 20;
@@ -87,12 +92,12 @@ function loadGame() {
     let cellStates = setupLevel(gridCells,level1Grid);
     const isPlayer = (cell) => cell =='p'
     let playerPosition = level1Grid.findIndex(isPlayer);
+    const scoreDisplay=document.querySelector("#score")
 
 
     // let playerPosition = 45;
 
     // gridCells[playerPosition].classList.add("player")
-    let score = 0
 
     document.addEventListener("keyup", (e) => {
         // console.log(e)
@@ -104,7 +109,7 @@ function loadGame() {
                 if (playerPosition < width || cellStates[playerPosition+inc].classList.contains('wall')) {
                     break;
                 } else {
-                    [playerPosition,score]=movePlayer(playerPosition,inc,cellStates,score)
+                    [playerPosition,score]=movePlayer(playerPosition,inc,cellStates, scoreDisplay)
                 }
                 break;
             case "ArrowDown": 
@@ -112,7 +117,7 @@ function loadGame() {
                 if (playerPosition > width*height - width || cellStates[playerPosition+inc].classList.contains('wall')) {
                     break;
                 } else {
-                    [playerPosition,score]=movePlayer(playerPosition, inc, cellStates,score)
+                    [playerPosition,score]=movePlayer(playerPosition, inc, cellStates, scoreDisplay)
                 }
                 break;
 
@@ -122,7 +127,7 @@ function loadGame() {
                 if (playerPosition % width == 0 || cellStates[playerPosition+inc].classList.contains('wall')) {
                     break;
                 } else {
-                    [playerPosition,score]=movePlayer(playerPosition, inc, cellStates,score)
+                    [playerPosition,score]=movePlayer(playerPosition, inc, cellStates, scoreDisplay)
                 }
                 break;
             
@@ -131,12 +136,13 @@ function loadGame() {
                 if (playerPosition % width == width -1 || cellStates[playerPosition+inc].classList.contains('wall')) {
                     break;
                 } else {
-                    [playerPosition,score]=movePlayer(playerPosition, inc, cellStates,score)
+                    [playerPosition,score]=movePlayer(playerPosition, inc, cellStates,scoreDisplay)
                 }
                 break;
 
             
         }
+        console.log(typeof score)
         console.log(score)
     })
 
